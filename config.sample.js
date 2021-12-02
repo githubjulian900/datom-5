c60e425c68df1a5c4b37af684e4941c73356dc46
Jelko Smidt <BWB45@OWSA.NL>
2021-12-02 19:46:40 +0100

    h

---
+++ config.sample.js
@@ -0,0 +1,52 @@
+module.exports = {
+	/* The token of your Discord Bot */
+	token: "OTE0ODAyNzkwMDcxNjkzMzEz.YaSWfQ.hR1X6qiw66LPtPS8mhAhIRRC8Sw",
+	/* For the support server */
+	support: {
+		id: "XXXXXXXXXXX", // The ID of the support server
+		logs: "XXXXXXXXXXX", // And the ID of the logs channel of your server (new servers for example)
+	},
+	/* Dashboard configuration */
+	dashboard: {
+		enabled: false, // whether the dashboard is enabled or not
+		secret: "XXXXXXXXXXX", // Your discord client secret
+		baseURL: "http://maxhgaming.c1.bic", // The base URl of the dashboard
+		logs: "XXXXXXXXXXX", // The channel ID of logs
+		port: 8080, // Dashboard port
+		expressSessionPassword: "XXXXXXXXXXX", // Express session password (it can be what you want)
+		failureURL: "http://.maxhgamingc1.biz" // url on which users will be redirected if they click the cancel button (discord authentication)
+	},
+	mongoDB: "mongodb://localhost:27017/AtlantaBot", // The URl of the mongodb database
+	prefix: "*", // The default prefix for the bot
+	/* For the embeds (embeded messages) */
+	embed: {
+		color: "#0091fc", // The default color for the embeds
+		footer: "Datom - Alle Rechten In Bezit " // And the default footer for the embeds
+	},
+	/* Bot's owner informations */
+	owner: {
+		id: "654754089909092362", // The ID of the bot's owner
+		name: "moi#4277" // And the name of the bot's owner
+	},
+	/* DBL votes webhook (optional) */
+	votes: {
+		port: 5000, // The port for the server
+		password: "XXXXXXXXXXX", // The webhook auth that you have defined on discordbots.org
+		channel: "XXXXXXXXXXX" // The ID of the channel that in you want the votes logs
+	},
+	/* The API keys that are required for certain commands */
+	apiKeys: {
+		// BLAGUE.XYZ: https://blague.xyz/
+		blagueXYZ: "XXXXXXXXXXX",
+		// FORTNITE TRN: https://fortnitetracker.com/site-api
+		fortniteTRN: "XXXXXXXXXXX",
+		// FORTNITE FNBR: https://fnbr.co/api/docs
+		fortniteFNBR: "XXXXXXXXXXX",
+		// DBL: https://discordbots.org/api/docs#mybots
+		dbl: "XXXXXXXXXXX",
+		// AMETHYSTE: https://api.amethyste.moe
+		amethyste: "XXXXXXXXXXX",
+		// SENTRY: https://sentry.io (this is not required and not recommended - you can delete the field)
+		sentryDSN: "XXXXXXXXXXX"
+	} 
+};
